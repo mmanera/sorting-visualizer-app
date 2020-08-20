6a998a75bb50f1dedf5d927a744de925d4d92d43
manera <manera.dev@gmail.com>
2020-08-20 11:33:52 +0200

    Updates

---
+++ precache-manifest.41638d206e647048558a89aa1e9349bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dac8cc5bd2f9a58ffdbeabf1e410bb27",
+    "url": "/sorting-visualizer-app/index.html"
+  },
+  {
+    "revision": "0746f870bfc9c6064760",
+    "url": "/sorting-visualizer-app/static/css/main.e2bd08f0.chunk.css"
+  },
+  {
+    "revision": "cde9c843322d486c0bc2",
+    "url": "/sorting-visualizer-app/static/js/2.16ee61c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sorting-visualizer-app/static/js/2.16ee61c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0746f870bfc9c6064760",
+    "url": "/sorting-visualizer-app/static/js/main.7529e726.chunk.js"
+  },
+  {
+    "revision": "0f08a5986dc34303ef86",
+    "url": "/sorting-visualizer-app/static/js/runtime-main.00ffb296.js"
+  }
+]);
